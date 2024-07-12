5d272e9f613e3b9f7c8d2c031b0a75a174727f43
JonerMello <lostuseranonymus@gmail.com>
2024-07-12 15:31:03 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,46 +4,53 @@
 if command -v python3.8.10 &>/dev/null; then
     echo "Python 3.8 já está instalado."
 else
-    # Atualizar os pacotes do sistema
-    sudo apt update
+    # Mensagem de alerta para o usuário
+    read -p "Uma nova versão do Python (3.8.10) será instalada. Deseja continuar? (s/n): " resposta
 
-    # Instalar as dependências necessárias
-    sudo apt install -y \
-        build-essential \
-        zlib1g-dev \
-        libncurses5-dev \
-        libgdbm-dev \
-        libnss3-dev \
-        libssl-dev \
-        libreadline-dev \
-        libffi-dev \
-        libsqlite3-dev \
-        wget \
-        libbz2-dev \
-        liblzma-dev
+    if [[ "$resposta" =~ ^[Ss]$ ]]; then
+        # Atualizar os pacotes do sistema
+        sudo apt update
 
-    # Baixar o código fonte do Python 3.8.10
-    cd /tmp
-    wget https://www.python.org/ftp/python/3.8.10/Python-3.8.10.tgz
+        # Instalar as dependências necessárias
+        sudo apt install -y \
+            build-essential \
+            zlib1g-dev \
+            libncurses5-dev \
+            libgdbm-dev \
+            libnss3-dev \
+            libssl-dev \
+            libreadline-dev \
+            libffi-dev \
+            libsqlite3-dev \
+            wget \
+            libbz2-dev \
+            liblzma-dev
 
-    # Extrair o arquivo baixado
-    tar -xf Python-3.8.10.tgz
+        # Baixar o código fonte do Python 3.8.10
+        cd /tmp
+        wget https://www.python.org/ftp/python/3.8.10/Python-3.8.10.tgz
 
-    # Navegar até o diretório extraído
-    cd Python-3.8.10
+        # Extrair o arquivo baixado
+        tar -xf Python-3.8.10.tgz
 
-    # Configurar o script de instalação
-    ./configure --enable-optimizations
+        # Navegar até o diretório extraído
+        cd Python-3.8.10
 
-    # Compilar e instalar o Python
-    make -j $(nproc)
-    sudo make altinstall
+        # Configurar o script de instalação
+        ./configure --enable-optimizations
 
-    # Verificar a instalação
-    python3.8 --version
+        # Compilar e instalar o Python
+        make -j $(nproc)
+        sudo make altinstall
+
+        # Verificar a instalação
+        python3.8 --version
+    else
+        echo "Instalação do Python 3.8.10 cancelada pelo usuário."
+        exit 1
+    fi
 fi
 
 # Criar e ativar um ambiente virtual para Python 3.8.10
 python3.8 -m venv env_python3.8
 source env_python3.8/bin/activate
-
